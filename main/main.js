569a06debd2a0e89d4d157f3767cce77fcfc9eb5
yinmuhuadaohugo <you@example.com>
2018-09-27 17:36:07 +0800

    init file

--- main/main.js
+++ main/main.js
@@ -1,5 +1,8 @@
 // Write your cade below:
 module.exports = function main() {
+	
+};
+module.exports.prototype.show = function(){
 	var a = prompt("请输入第一个数");
 	var b = prompt("请输入第二个数");
 	var c = a%b;
